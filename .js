0867b86a33ab44c9ca4a49051cf0e06802892c44
s111111520 <126218302+s111111520@users.noreply.github.com>
2023-03-27 10:27:12 +0800

    Delete .js

--- .js
+++
@@ -1,8 +0,0 @@
-function isPrime(n){
-    for(let i=2; i<n; i++)} {
-        if (n%i ===0) return false
-        return ture
-    }
-
-    console.log('isPrime(7)=', isPrime(7))
-    console.log('isPrime(9)=', isPrime(9))
