05bd266579f65346d0592ee56ac011fabd6a981d
s111111520 <126218302+s111111520@users.noreply.github.com>
2023-03-27 09:53:42 +0800

    Update .js

--- .js
+++ .js
@@ -1,1 +1,8 @@
+function isPrime(n){
+    for(let i=2; i<n; i++)} {
+        if (n%i ===0) return false
+        return ture
+    }
 
+    console.log('isPrime(7)=', isPrime(7))
+    console.log('isPrime(9)=', isPrime(9))
